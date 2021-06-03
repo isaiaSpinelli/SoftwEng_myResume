d961bfd27773b16ef9b198b5f1f5e74e81d20042
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2021-06-03 14:11:45 +0200

    try test skipped

--- tests/mul.test.js
+++ tests/mul.test.js
@@ -8,8 +8,8 @@ test('2 * 0 = 0', () => {
     expect(mul(2, 0)).toEqual(0);
 });
   
-test('2 * -1 = -2', () => {
-	expect(mul(2, -1)).toEqual(2);
+test.skip('2 * -1 = -2', () => {
+	expect(mul(2, -1)).toEqual(-2);
 });
 
 test('-2 * -1 = 2', () => {
