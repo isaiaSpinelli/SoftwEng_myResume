e77bd755017fd74c7f7ff87a5b6fa9a9254ea780
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2021-06-03 08:39:40 +0200

    exercise on testing with jest

---
+++ tests/mul.test.js
@@ -0,0 +1,18 @@
+const {mul} = require('./mul');
+
+test('1 * 1 = 1', () => {
+ expect(mul(1, 1)).toBe(1);
+});
+
+test('2 * 0 = 0', () => {
+    expect(mul(2, 0)).toEqual(0);
+});
+  
+test.skip('2 * -1 = -2', () => {
+	expect(mul(2, -1)).toEqual(-2);
+});
+
+test('-2 * -1 = 2', () => {
+	expect(mul(-2, -1)).toEqual(2);
+});
+
